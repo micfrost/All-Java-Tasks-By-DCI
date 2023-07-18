6f2d103ec4b7cd8b600b6c1e1e5f60472050e364
Michal Frost <michal.frost@dci-student.org>
2023-07-18 15:36:45 +0200

    initial commit

---
+++ src/D16_T1_if.java
@@ -0,0 +1,10 @@
+public class D16_T1_if {
+    public static void main(String[] args) {
+        int givenNumber = 2;
+        if (givenNumber % 2 == 0) {
+            System.out.println(givenNumber * 10);
+        } else {
+            System.out.println(givenNumber);
+        }
+    }
+}
---
+++ src/D16_T2_ifElse.java
@@ -0,0 +1,10 @@
+public class D16_T2_ifElse {
+    public static void main(String[] args) {
+        int receivedNumber = Integer.parseInt(args[0]);
+        if (receivedNumber >= 0) {
+            System.out.println("The number is positive");
+        } else {
+            System.out.println("The number is negative");
+        }
+    }
+}
