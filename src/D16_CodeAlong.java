c94a30b642ef43d7e41ac7e3fdf0774ee2b0c9e5
Michal Frost <michal.frost@dci-student.org>
2023-07-18 15:32:25 +0200

    initial commit

---
+++ src/D16_CodeAlong.java
@@ -0,0 +1,16 @@
+public class D16_CodeAlong {
+    public static void main(String[] args) {
+        int covidCases = 50;
+        boolean ishomeinconstrction = true;
+        if (covidCases >= 45) {
+            System.out.println("I am going home.");
+//Nested if
+            if (ishomeinconstrction) {
+                System.out.println("oh no the home it is construction");
+            }
+        } else {
+            System.out.println("I am going to college.");
+        }
+        System.out.println("Nice talking with you.");
+    }
+}
