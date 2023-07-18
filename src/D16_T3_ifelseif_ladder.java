76c92ed8c0a505b268d9c8a6629cb9c498b8e3f2
Michal Frost <michal.frost@dci-student.org>
2023-07-18 14:49:25 +0200

    init

--- src/D16_T3_ifelseif_ladder.java
+++ src/D16_T3_ifelseif_ladder.java
@@ -1,6 +1,16 @@
 public class D16_T3_ifelseif_ladder {
     public static void main(String[] args) {
+        int value1 = Integer.parseInt(args[0]);
+        int value2 = Integer.parseInt(args[1]);
 
+        if (value1>value2) {
+            System.out.println(value1 + " is grater that " + value2);
+        }
+        if (value1<value2) {
+            System.out.println(value2 + " is grater that " + value1);
+        } else {
+            System.out.println("Both values are equals");
+        }
     }
 
 }
