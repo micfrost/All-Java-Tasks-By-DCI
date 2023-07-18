a2aa5a542408aa28035bb3c09fce877e2fb557f6
Michal Frost <michal.frost@dci-student.org>
2023-07-18 14:25:04 +0200

    D16_t

---
+++ src/D16_Task5_Switch.java
@@ -0,0 +1,23 @@
+public class D16_Task5_Switch {
+    public static void main(String[] args) {
+        String userType = args[0];
+
+        switch (userType) {
+            case "superAdmin":
+                System.out.println("Logged in as a Super Admin, with all the super powers.");
+                break;
+            case "admin":
+                System.out.println("Logged in as an Admin, with all the admin powers.");
+                break;
+            case "user":
+                System.out.println("Logged in a regular folk.");
+                break;
+            case "guest":
+                System.out.println("Logged in as guest. Would you mind signing in?");
+                break;
+            case "default case":
+                System.out.println("User type not recognized!");
+                break;
+        }
+    }
+}
