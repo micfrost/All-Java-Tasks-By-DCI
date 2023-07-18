b18e9f79bc8c23dcc6427e00b06eb8da242f15a3
Michal Frost <michal.frost@dci-student.org>
2023-07-18 15:22:31 +0200

    initial commit

---
+++ src/D16_T4_nestedIf.java
@@ -0,0 +1,17 @@
+public class D16_T4_nestedIf {
+    public static void main(String[] args) {
+        int user = Integer.parseInt(args[0]);
+        int id = Integer.parseInt(args[1]);
+
+        if (user == 200) {
+            if (id == 150) {
+                System.out.println("Valid ID number.");
+            } else {
+                System.out.println("Invalid ID number.");
+            }
+        } else {
+            System.out.println("You need to be an admin to perform operations.");
+        }
+
+    }
+}
