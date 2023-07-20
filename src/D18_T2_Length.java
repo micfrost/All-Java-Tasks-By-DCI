793c46c2f1e284497455c00b4398e79a5c6d536a
Michal Frost <michal.frost@dci-student.org>
2023-07-20 14:28:05 +0200

    initial commit

---
+++ src/D18_T2_Length.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class D18_T2_Length {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Give me a String: ");
+        String givenWord = scanner.nextLine();
+        System.out.println();
+
+        int lenghtOfWord = givenWord.length();
+        if (lenghtOfWord % 2 == 1) {
+            System.out.println("\"" + givenWord + "\"" + " --> " + "\"odd\"");
+        } else {
+            System.out.println("\"" + givenWord + "\"" + " --> " + "\"even\"");
+        }
+    }
+}
