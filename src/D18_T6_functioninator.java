5a8c1865b592b4ab8ddb3eeae1f4b4fb6e7dbdf0
Michal Frost <michal.frost@dci-student.org>
2023-07-23 23:29:13 +0200

    initial commit_consonant

--- src/D18_T6_functioninator.java
+++ src/D18_T6_functioninator.java
@@ -5,7 +5,13 @@ public class D18_T6_functioninator {
 
         System.out.println("Write a single word: ");
         String givenWord = scanner.nextLine();
-        System.out.println(givenWord + "inator" + " " + givenWord.length() + "000");
+        char lastChar = givenWord.charAt(givenWord.length() - 1);
+        boolean isEndConsonant = (lastChar == 'a' || lastChar == 'e' || lastChar == 'o' || lastChar == 'u' || lastChar == 'i' || lastChar == 'y');
+        if (isEndConsonant) {
+            System.out.println("inatorInator(\"" + givenWord + "\") --> " + "\"" + givenWord + "-inator" + givenWord.length() + "000\"");
+        } else {
+            System.out.println("inatorInator(\"" + givenWord + "\") --> " + "\"" + givenWord + "inator" + givenWord.length() + "000\"");
+        }
 
     }
 }
