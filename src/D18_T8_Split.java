e4510670b9d89f44d137ff44f0c42344c94a8b09
MicFro <michalfrost71@gmail.com>
2023-07-31 17:51:06 +0200

    initial commit

---
+++ src/D18_T8_Split.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class D18_T8_Split {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Provide a sentence: ");
+        String givenInput = scanner.nextLine();
+
+        String firstWord = givenInput.substring(0,givenInput.indexOf(" ")) + " ";
+        String output = firstWord.repeat(2) + givenInput;
+        System.out.println("input --> output");
+        System.out.println(givenInput + " --> " + output);
+    }
+}
