b1ddf73c3aca38ee17e39b6cb65064f6c639cae8
MicFro <michalfrost71@gmail.com>
2023-08-05 15:32:49 +0200

    initial commit

---
+++ src/D18_T9_Join.java
@@ -0,0 +1,8 @@
+public class D18_T9_Join {
+    public static void main(String[] args) {
+        String[] inputList = {"bananas", "apples", "milk"};
+        System.out.println(inputList[0] + ", " + inputList[1] + ", " + inputList[2]);
+        String outputList = inputList[0] + "," + inputList[1] + "," + inputList[2];
+        System.out.println(outputList);
+    }
+}
