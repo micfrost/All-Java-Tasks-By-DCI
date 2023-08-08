2ba75ce287df84831f3795bdff95d0a1db05e4cd
MicFro <michalfrost71@gmail.com>
2023-08-08 11:30:29 +0200

    uodate

--- src/D20/D20_T1/ArithmeticCalculator.java
+++ src/D20/D20_T1/ArithmeticCalculator.java
@@ -1,28 +1,28 @@
 package D20.D20_T1;
 
 public class ArithmeticCalculator {
-    double addition(double value1, double value2) {
+    double add(double value1, double value2) {
         System.out.print("Addition: ");
         double result = value1+value2;
         System.out.print(value1 + " + " + value2 + " = ");
         return result;
     }
 
-    double subtraction(double value1, double value2) {
+    double subtract(double value1, double value2) {
         System.out.print("Subtraction: ");
         double result = value1-value2;
         System.out.print(value1 + " - " + value2 + " = ");
         return result;
     }
 
-    double multiplication(double value1, double value2) {
+    double multiply(double value1, double value2) {
         System.out.print("Multiplication: ");
         double result = value1*value2;
         System.out.print(value1 + " * " + value2 + " = ");
         return result;
     }
 
-    double division(double value1, double value2) {
+    double divide(double value1, double value2) {
         System.out.print("Division: ");
         double result = value1/value2;
         System.out.print(value1 + " / " + value2 + " = ");
