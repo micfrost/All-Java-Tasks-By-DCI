219676ab3cc685e8998157080bc03cea1247400e
MicFro <michalfrost71@gmail.com>
2023-08-09 12:58:29 +0200

    init commit

---
+++ src/D21/D21_CA_PassingByValue/Person.java
@@ -0,0 +1,22 @@
+package D21.D21_CA_PassingByValue;
+
+public class Person {
+    String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return "Person [name=" + name + "]";
+    }
+
+}
