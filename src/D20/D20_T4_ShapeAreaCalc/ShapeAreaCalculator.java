32d2fdbc4eab23036c85af66ae002d2de67d6172
MicFro <michalfrost71@gmail.com>
2023-08-09 09:03:15 +0200

    update

--- src/D20/D20_T4_ShapeAreaCalc/ShapeAreaCalculator.java
+++ src/D20/D20_T4_ShapeAreaCalc/ShapeAreaCalculator.java
@@ -6,12 +6,18 @@ public class ShapeAreaCalculator {
         return Math.PI * radius * radius;
     }
 
-    double calculateArea(float length, float width) {
+    double calculateArea(double length, double width) {
         return length * width;
     }
 
-    double calculateArea(double base, double height) {
-        return 0.5 * base * height;
+    double calculateArea(boolean isTriangle, double base, double height) {
+        double result;
+        if (isTriangle == true) {
+            result = 0.5 * base * height;
+        } else {
+            result = 0;
+        }
+        return result;
     }
 
 }
