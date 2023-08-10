44056d90838e0b35f226070fcf15b32815081e5d
MicFro <michalfrost71@gmail.com>
2023-08-10 15:26:22 +0200

    init commit

---
+++ src/D22/D22_T1_WhileLoop/Main.java
@@ -0,0 +1,20 @@
+package D22.D22_T1_WhileLoop;
+
+public class Main {
+    public static void main(String[] args) {
+        NumberCounter nCounter = new NumberCounter();
+        nCounter.countAndDisplay(1, 10);
+    }
+
+    public static class NumberCounter {
+
+        public void countAndDisplay(int start, int end) {
+
+            while (start <= end) {
+                System.out.println(start);
+                start ++;
+            }
+        }
+    }
+
+}
