36c068b06839b2028aa73d53f8a7edebf4a0becf
MicFro <michalfrost71@gmail.com>
2023-08-16 14:08:18 +0200

    init commit

---
+++ src/D23_For_Loops/D23_T2_For_EvenNum/Main.java
@@ -0,0 +1,23 @@
+package D23_For_Loops.D23_T2_For_EvenNum;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println();
+        System.out.println("A program that prints all even numbers between a specified range of integers.");
+        System.out.println("Enter the starting value of the range");
+        int start = Integer.valueOf(scanner.nextLine());
+        System.out.println("Enter the ending value of the range");
+        int end = Integer.valueOf(scanner.nextLine());
+        System.out.print("All even numbers between " + start + " and " + end + " are : ");
+        for (int i = start; i <= end; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        scanner.close();
+    }
+}
