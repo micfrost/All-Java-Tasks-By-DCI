d8dc0f5a96bdd087fd9465f0e0d63d8678e0745b
MicFro <michalfrost71@gmail.com>
2023-08-16 13:38:45 +0200

    init commit

---
+++ src/D23_For_Loops/D23_T1_Loop_Factrorial/Main.java
@@ -0,0 +1,31 @@
+package D23_For_Loops.D23_T1_Loop_Factrorial;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println();
+        System.out.println("A program that calculates the factorial of a given positive integer n.");
+        System.out.println("Enter a value of n: ");
+
+        int n = Integer.valueOf(scanner.nextLine());
+
+        if (n<0) {
+            System.out.println("The number you entered is not a positive number.");
+        } else {
+
+            int factorialValue = 1;
+
+            for (int i = 1; i <= n; i++) {
+                factorialValue = factorialValue * i;
+                System.out.println("Result in a Loop for i= " + i + " is : " + factorialValue);
+            }
+            System.out.println("End Result. Factorial of " + n + " is: " + factorialValue);
+
+            scanner.close();
+        }
+    }
+}
