1fb44eccd8dc98c75a612f0fe856240ebd88e834
MicFro <michalfrost71@gmail.com>
2023-08-17 16:07:11 +0200

    init commit

---
+++ src/D24_logicalOperators/D24_T4_Ternary/Main.java
@@ -0,0 +1,23 @@
+package D24_logicalOperators.D24_T4_Ternary;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Discount Calculator with Ternary Operator");
+        System.out.println("A Java program to calculate the total cost of an order after applying a discount based on the order amount.");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the order amount: ");
+        double givenOrderAmount = scanner.nextDouble();
+
+        double totalCost = givenOrderAmount > 100 ?
+                givenOrderAmount - givenOrderAmount * 0.1 :
+                givenOrderAmount;
+
+        System.out.println("Total cost: " + totalCost + " $");
+
+    }
+
+}
---
+++ src/D24_logicalOperators/D24_T5_Ternary/Main.java
@@ -0,0 +1,28 @@
+package D24_logicalOperators.D24_T5_Ternary;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Grade Calculator with Ternary Operator");
+        System.out.println("Java program to calculate the letter grade for a student based on their numerical score.");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the numerical score: ");
+        int givenNumericalScore = scanner.nextInt();
+
+        char letterGrade = givenNumericalScore >= 90 ? 'A'
+                : givenNumericalScore >= 80 ? 'B'
+                : givenNumericalScore >= 70 ? 'C'
+                : givenNumericalScore >= 60 ? 'D'
+                : 'F';
+
+
+        System.out.println("Your given numerical score is " + givenNumericalScore + " and a letter grade is: " + letterGrade + ".");
+
+
+    }
+
+}
