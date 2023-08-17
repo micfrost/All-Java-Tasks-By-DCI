f614651f2111004c248ea5885edc5773112fc6d5
MicFro <michalfrost71@gmail.com>
2023-08-17 10:30:56 +0200

    init commit

---
+++ src/D24_logicalOperators/C24_CA_TernaryOpe/Main.java
@@ -0,0 +1,16 @@
+package D24_logicalOperators.C24_CA_TernaryOpe;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a type ");
+        int temp = scanner.nextInt();
+
+        String weather = (temp >= 40) ? "Very hot!" : ( temp >= 25) ? "Normal Hot!" : (temp >= 0) ? "Cold!" : "Frost";
+        System.out.println(weather);
+
+    }
+}
