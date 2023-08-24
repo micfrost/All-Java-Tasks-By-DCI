163ea09b0f9c92ffcc685e097009b8bfb9698e5a
MicFro <michalfrost71@gmail.com>
2023-08-24 20:48:37 +0200

    init commit

--- src/D28_ArrayList/D28_T1_ShoppingList/App.java
+++ src/D28_ArrayList/D28_T1_ShoppingList/App.java
@@ -1,6 +1,52 @@
 package D28_ArrayList.D28_T1_ShoppingList;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
+
+    public void run() {
+
+        ArrayList<String> arrList = new ArrayList<>();
+
+
+        System.out.println("Menu:");
+        System.out.println("1. Add an item");
+        System.out.println("2. Remove an item");
+        System.out.println("3. Display the list");
+        System.out.println("4. End");
+        System.out.print("Enter selected number: ");
+
+
+        int chosenOption;
+        do {
+
+            Scanner scanner = new Scanner(System.in);
+
+            chosenOption = Integer.valueOf(scanner.nextLine());
+
+            switch (chosenOption) {
+                case 1:
+                    System.out.println(1);
+                    break;
+                case 2:
+                    System.out.println(2);
+                    break;
+                case 3:
+                    System.out.println(3);
+                    break;
+                case 4:
+                    System.out.println("Exit the program");
+                    break;
+                default:
+                    System.out.println("Try one more time.");
+                    break;
+            }
+        } while (chosenOption != 4);
+    }
+
+
+    }
 
 
 }
--- src/D28_ArrayList/D28_T1_ShoppingList/Main.java
+++ src/D28_ArrayList/D28_T1_ShoppingList/Main.java
@@ -1,6 +1,6 @@
 package D28_ArrayList.D28_T1_ShoppingList;
 
-import D27_MultidimensionalArrays.D27_T4_2DArray_StudentGradeTracker.App;
+
 
 public class Main {
     public static void main(String[] args) {
