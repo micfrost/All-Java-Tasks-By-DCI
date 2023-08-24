5702097064339916ac37e2e10c35fb71c92c12bf
MicFro <michalfrost71@gmail.com>
2023-08-24 15:21:14 +0200

    init commit

---
+++ src/D28_ArrayList/D28_T1_ShoppingList/App.java
@@ -0,0 +1,6 @@
+package D28_ArrayList.D28_T1_ShoppingList;
+
+public class App {
+
+
+}
---
+++ src/D28_ArrayList/D28_T1_ShoppingList/Main.java
@@ -0,0 +1,12 @@
+package D28_ArrayList.D28_T1_ShoppingList;
+
+import D27_MultidimensionalArrays.D27_T4_2DArray_StudentGradeTracker.App;
+
+public class Main {
+    public static void main(String[] args) {
+
+        App app = new App();
+        app.run();
+
+    }
+}
