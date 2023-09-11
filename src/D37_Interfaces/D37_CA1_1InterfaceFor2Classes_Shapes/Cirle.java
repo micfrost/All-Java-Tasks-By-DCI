53d3d1a061f60a6a6521ad06fd515f6dc2104ff9
MicFro <michalfrost71@gmail.com>
2023-09-11 10:44:00 +0200

    init commit

---
+++ src/D37_Interfaces/D37_CA1_1InterfaceFor2Classes_Shapes/Cirle.java
@@ -0,0 +1,9 @@
+package D37_Interfaces.D37_CA1_1InterfaceFor2Classes_Shapes;
+
+public class Cirle implements Shape  {
+    @Override
+    public void draw() {
+        System.out.println("Drawing a circle.");
+
+    }
+}
