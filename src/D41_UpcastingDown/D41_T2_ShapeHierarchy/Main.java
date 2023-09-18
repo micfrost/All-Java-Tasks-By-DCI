bcd0d0b0de388bcae0e910163df0181d1921421a
MicFro <michalfrost71@gmail.com>
2023-09-18 14:27:34 +0200

    init commit

--- src/D41_UpcastingDown/D41_T2_ShapeHierarchy/Main.java
+++ src/D41_UpcastingDown/D41_T2_ShapeHierarchy/Main.java
@@ -1,4 +1,42 @@
 package D41_UpcastingDown.D41_T2_ShapeHierarchy;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("- - -");
+        System.out.println("Upcasting and Downcasting - Shape Hierarchy");
+        System.out.println("A Java program that demonstrates upcasting (assigning a subclass object to a superclass reference) and downcasting (casting a superclass reference to a subclass reference) by modeling a hierarchy of geometric shapes. ");
+        System.out.println("- - - ");
+
+        Shape shape1 = new Circle("Red", 5);
+        Shape shape2 = new Rectangle("Blue", 5.1, 5.2);
+        Shape shape3 = new Triangle("Green", 5.1, 5.2);
+
+        shape1.calculateArea();
+        shape2.calculateArea();
+        shape3.calculateArea();
+
+//        Downcasting to access the specific attributes and behaviors
+        System.out.println("- - - ");
+        if (shape1 instanceof Circle) {
+            Circle circle1 = (Circle) shape1;
+            System.out.println("The Radius: " + circle1.getRadius());
+        }
+
+        if (shape2 instanceof Rectangle) {
+            Rectangle rectangle1 = (Rectangle) shape2;
+            System.out.println("The Width: " + rectangle1.getWidth());
+            System.out.println("The Height: " + rectangle1.getHeight());
+        }
+
+        if (shape3 instanceof Triangle) {
+            Triangle triangle1 = (Triangle) shape3;
+            System.out.println("The Base: " + triangle1.getBase());
+            System.out.println("The Height: " + triangle1.getBaseHeight());
+        }
+
+
+    }
 }
+
--- src/D41_UpcastingDown/D41_T2_ShapeHierarchy/Shape.java
+++ src/D41_UpcastingDown/D41_T2_ShapeHierarchy/Shape.java
@@ -1,4 +1,70 @@
 package D41_UpcastingDown.D41_T2_ShapeHierarchy;
+abstract class Shape {
+    private String color;
+    public Shape(String color) {
+        this.color = color;
+    }
+    public String getColor() {
+        return color;
+    }
+    abstract void calculateArea();
+}
 
-public class Shape {
+class Circle extends Shape{
+    private double radius;
+    public double getRadius() {
+        return radius;
+    }
+    public Circle(String color, double radius) {
+       super(color);
+        this.radius = radius;
+    }
+    @Override
+    void calculateArea() {
+        System.out.printf("The area of a %s circle with a radius %.2f is: %.2f\n" ,getColor(), radius, (Math.PI*radius*radius));
+    }
+}
+
+class Rectangle extends Shape{
+    private double width;
+    private double height;
+    public double getWidth() {
+        return width;
+    }
+    public double getHeight() {
+        return height;
+    }
+    public Rectangle(String color, double width, double height) {
+        super(color);
+        this.width = width;
+        this.height = height;
+    }
+    @Override
+    void calculateArea() {
+        System.out.println("The are of a " +getColor() + " rectangle with a width " + width + " and height " + height + " is: " + (width*height));
+    }
+}
+class Triangle extends Shape{
+    private double base;
+    private double baseHeight;
+    public double getBase() {
+        return base;
+    }
+
+    public double getBaseHeight() {
+        return baseHeight;
+    }
+
+    public Triangle(String color, double base, double baseHeight) {
+        super(color);
+        this.base = base;
+        this.baseHeight = baseHeight;
+    }
+
+    @Override
+    void calculateArea() {
+
+            System.out.println("The are of a " +getColor() + " triangle with a base" + base + " and Height " + baseHeight + " is: " + 0.5*(base*baseHeight));
+
+    }
 }
