6c5a7fd4be9af4e715a71f3780df338bcda5caee
micfro <michalfrost71@gmail.com>
2023-11-26 21:30:39 +0100

    commit init

--- src/D73_SwitchExpressions/T_1_CoffeShop/micfro1/Main.java
+++ src/D73_SwitchExpressions/T_1_CoffeShop/micfro1/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void order(String choiceCoffee, String choiceSize) {
 
         String coffeAndsize = choiceCoffee + choiceSize;
-        double s1 = 1.25;
+        double s1 = 100.25;
         double s2 = 1.50;
 
         double l1 = 2.25;
